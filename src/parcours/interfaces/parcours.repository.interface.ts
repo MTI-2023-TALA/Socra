d25c7642d6f2179cc74dc9c5929c69c27084455c
Alan Schaeffer <alan.schaeffer29@gmail.com>
2022-06-09 12:00:27 +0200

    fix(interface): removed and changed wrong interfaces

--- src/parcours/interfaces/parcours.repository.interface.ts
+++ src/parcours/interfaces/parcours.repository.interface.ts
@@ -6,7 +6,6 @@ import { UpdateParcoursDto } from '../../dto/update-parcours.dto';
 export interface ParcoursRepositoryInterface {
   getAllParcours(): Promise<WithId<Document>[]>;
   getParcoursById(id: string): Promise<WithId<Document> | null>;
-  getParcoursByKeywords(keywords: string[]): Promise<WithId<Document>[] | null>;
   addParcours(createParcoursDto: CreateParcoursDto): Promise<InsertOneResult>;
   updateParcours(id: string, updateParcoursDto: UpdateParcoursDto): Promise<UpdateResult>;
 }
